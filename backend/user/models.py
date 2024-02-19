0c1b9146c296e1286fc2127da9d068e65dc56dc5
hemanthreddybhumana <159986565+hemanthreddybhumana@users.noreply.github.com>
2024-02-19 17:59:35 +0530

    my first commit

---
+++ backend/user/models.py
@@ -0,0 +1,10 @@
+# user/models.py
+from djongo import models
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
